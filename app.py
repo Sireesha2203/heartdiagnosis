2754563caff91f2fa3a06a588db5b06fb650831c
Sireesha2203 <119487435+Sireesha2203@users.noreply.github.com>
2025-02-06 23:48:44 +0530

    host commit 3

--- app.py
+++ app.py
@@ -36,20 +36,22 @@ def predict():
 
     # Convert data to DataFrame for prediction
     filtered_data = {key: value for key, value in data.items() if key in required_fields}
-
+    if filtered_data['Sex']=='Male':
+        filtered_data['Sex']=1
+    else:
+        filtered_data['Sex']=0
+    if filtered_data['ExerciseAngina']=='Yes':
+        filtered_data['ExerciseAngina']=1
+    else:
+        filtered_data['ExerciseAngina']=0
     # Convert filtered data to DataFrame
     df = pd.DataFrame([filtered_data])
 
     # Select model
-    # model_name = data.get('model')  # Default to random forest if not provided
-    # if model_name not in models:
-    #     return jsonify({"error": f"Model '{model_name}' not found"}), 404
-    # model_name='lgbm'
-    # Select model (default to logistic regression if not provided)
-    model_name = data.get('model', 'logistic_regression')  # Default to 'logistic_regression'
-    
+    model_name = data.get('model')  # Default to random forest if not provided
     if model_name not in models:
-        return jsonify({"error": f"Model '{model_name}' not found"}), 404 
+        return jsonify({"error": f"Model '{model_name}' not found"}), 404
+    model_name='lgbm'
 
     # Get model
     model = models[model_name]
